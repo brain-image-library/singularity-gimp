9c65c38e6683b0b8a1f2115f62a82dcd763eb668
icaoberg <icaoberg@cmu.edu>
2024-04-18 18:20:17 -0400

    Update test.sh

--- 2.10/test.sh
+++ 2.10/test.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 tree .
-
-echo -e '"Spartans! Ready your breakfast and eat hearty, for tonight, we dine in hell!"' | singularity exec singularity-gimp-2.10.sif gimp -d peek -a c -s 40x11
